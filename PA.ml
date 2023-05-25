82010aafbd309f653fe762b3dbc74176883fb913
Pereira3 <95077144+Pereira3@users.noreply.github.com>
2023-05-25 01:43:03 +0100

    Add files via upload

--- PA.ml
+++ PA.ml
@@ -16,10 +16,25 @@ let rec eq n =
 		(match (Z.to_int n) with
 		| 0 -> Z.one
 		| 1 -> Z.one
-		| _ -> ( let res =  Z.div (Z.add (Z.mul(Z.add (Z.mul (n)(Z.of_int 2)) (Z.one))( eq(Z.sub(n)(Z.one)) )) (Z.mul(Z.sub (Z.mul (n)(Z.of_int 3)) (Z.of_int 3))( eq(Z.sub(n)(Z.of_int 2)) ))) (Z.add n (Z.of_int 2)) in
+		| _ -> ( let res = Z.add ( eq(n-1) eq(n-2) ) in
 		Hashtbl.add ht n res; res))
 	else Hashtbl.find ht n
 
 (*Conversão do tipo int para Z.int e apresenta o valor final*)
 let () = Z.print(eq (Z.of_int num));
 print_string("\n")
+
+(*Após a inicialização do programa, o mesmo fica à espera que o utilizador introduza um valor numérico.
+	Após a introdução desse valor numérico, guardado posteriormente na variável num, o programa certifica-se de que o valor de num é válido para o intervalo definido.
+	De modos ao programa ser mais eficiente e mais rápido é criada um hashtable e de modo recursivo, os valores correspondentes aos resultados da fórmula dentro da função recursiva vão sendo guardados na hashtable.
+	Para efeitos de rapidez, quando num é 1 ou 0 o programa dá logo o output direto, visto que para ambos os casos o resultado é 1.
+	Caso um valor já tenha sido calculado, o programa apenas recorre à hashtable e procura o resultado, prosseguindo com a fórmula ou prosseguindo assim para a apresentação do resultado.
+	
+	Caso o utilizador tenha escrito 3
+	Inicialmente é corrida a fórmula e fica (7*eq2 + 6*eq1)/5
+	De seguinda, como eq1 é 1, [7*((5*eq1 + 3*eq0)/4) +6]/5
+	Como eq0 é 1,  [7*((5*1+3*1)/4) +6]/5 
+	Resultado: 4
+
+	Conforme o programa vai calculando, vai guardado os resultados das operações efetuadas.
+	No exemplo anterior, conforme o programa foi percorrendo o eq de 2, 1 e 0, foi guardando os seus respetivos resultados na hashtable, visto que 1 e 0 não chegam à terceira parte do ciclo if, a hashtable apenas ficou com o valor de eq2 guardado.*)
--- PB.ml
+++ PB.ml
@@ -1,16 +1,73 @@
-(*Ler as variáveis, nfatias para efeito de calculos, ntlinhas para a quantidade de linhas da tabela*)
-let nfatias = read_int()
-let ntlinhas = read_int()
-(*Verificar se os valores são válidos*)
-let () = if nfatias < 0 || ntlinhas < 0 || nfatias >= 10000 || ntlinhas >= 10000 || nfatias <= ntlinhas then invalid_arg "Valor inserido inválido!"
+(*Ricardo Pereira nº 48554*)
+(*https://v2.ocaml.org/api/Stdlib.html*)
+(*Leitura dos nº inteiros inseridos*)
+let num_fatias = read_int()
+let num_precos = read_int()
 
-(*Array de 2 dimensões, n linhas 2 colunas, inicializado com tudo a 0*)
-let tabelaPQ = Array.make_matrix ntlinhas 2 0
-(*Inserção dos valores na matrix apartir de tuplos*)
+(*Validação dos valores inseridos*)
+let () = if num_fatias < 0 || num_precos < 0 || num_fatias > 10000 || num_precos > 10000 || num_fatias < num_precos then invalid_arg "Valores inseridos inválidos!"
+
+(*Guardar valores dos preços, caso num_fatia tenha algum valor saltado, este ficará com o valor num_preço inicializado a 0*)
+let array_precos = Array.make (num_fatias+1) 0
+(*Array que futuramente terá os valores máximos para cada valor de num_fatias guardado em cada um dos seus index*)
+let array_vm = Array.make (num_fatias+1) 0
+(*Valor máximo utilizado futuramente para a comparação dos valores sucessivos relativos aos preços por num_fatias*)
+let valor_max = ref 0
+
+(*Ciclo para introduzir os valores no array, como o array começa em 0 o index será o valor1, sendo o primeiro valor 0.
+O Ciclo é efetuado tantas vezes como num_precos-1, isto porque apenas as fatias com preço importam, e como o ciclo começa em 0
+num_preco-1 será a quantidade de vezes suficientes para preencher o array.*)
+let () = 
+	for i = 0 to num_precos-1 do 
+		let valor1, valor2 = 
+			Scanf.scanf " %d %d" (fun valor1 valor2 -> (valor1, valor2)) in
+			array_precos.(valor1) <- valor2;
+	done
+
+(*O Ciclo será efetuado tantas vezes como a quantidade de fatias.
+Após a execução do primeiro for, é devolvido o valor da variável relativa ao valor máximo e será inserido no array dos valores máximos.
+Após a execução do segundo for, é devolvido o valor da variável relativa ao valor máximo da variável num_fatias, o que será por consequência
+o valor máximo para as fatias inseridas como input.*)
+let rec lucro_max array_precos num_fatias =
+	for i = 1 to num_fatias do 
+		for j = 1 to i do
+			valor_max := max (!valor_max) (array_precos.(j) + array_vm.(i-j));
+		done; array_vm.(i) <- !valor_max;
+	done; array_vm.(num_fatias)
+
+(*Chamada da função e apresentação do respetivo Output*)
 let () =
-	for i = 0 to (ntlinhas-1) do
-		let (v1, v2) = Scanf.scanf " %d %d" (fun a b -> (a, b)) in
-    tabelaPQ.(i).(0) <- v1;
-		tabelaPQ.(i).(1) <- v2;
-  done;
+	Printf.printf "%d\n" (lucro_max array_precos num_fatias)
 
+(*Supondo que é inserido como input os seguintes valores:
+	6
+	4
+	1 2
+	2 4
+	4 5
+	6 7
+	
+	Inicialmente o programa verificar os dados inseridos e vai criar a variável valor_max e ambos os arrays (array_precos e array_vm).
+	De seguida insere os valores relativos aos preços no index correspondente ao tamanho da fatia:
+	| 0 | 1 | 2 | 3 | 4 | 5 | 6 |
+	| 0 | 2 | 4 | 0 | 5 | 0 | 7 |
+
+	Em seguida percorre os ciclos for de modos a achar o valor máximo para cada tamanho de fatia.
+	inserindo posteriormente esse valor no array_vm com o index esperado (numero de fatias):
+		- valor_max = max 0 2+0 2 = 2 -> array_vm.(1) = 2 -> Valor máximo para 1 fatia;
+	 	- valor_max = max 2 2+2 = 4 -> array_vm.(2) = 4 -> Soma de 2 fatias de tamanho 1;
+	 	- valor_max = max 4 4+0 = 4 -> array_vm.(2) = 4 -> Valor máximo das fatias de tamanho 2 por definição;
+		...
+		- valor_max = max 10 2+10 = 12 -> array_vm.(6) = 12 -> Soma entre uma fatia de tamanho 1 e o valor máximo obtido a partir de fatias de tamanho 5;
+		- valor_max = max 12 4+8 = 12 -> array_vm.(6) = 12 -> Soma entre uma fatia de tamanho 2 e o valor máximo obtido a partir de fatias de tamanho 4;
+		- valor_max = max 12 0+6 = 12 -> array_vm.(6) = 12 -> Soma entre uma fatia de tamanho 3 e o valor máximo obtido a partir de fatias de tamanho 3;
+		- valor_max = max 12 5+4 = 12 -> array_vm.(6) = 12 -> Soma entre uma fatia de tamanho 4 e o valor máximo obtido a partir de fatias de tamanho 2;
+		- valor_max = max 12 0+2 = 12 -> array_vm.(6) = 12 -> Soma entre uma fatia de tamanho 5 e o valor máximo obtido a partir de fatias de tamanho 1;
+		- valor_max = max 12 7+0 = 12 -> array_vm.(6) = 12 -> Valor máximo das fatias de tamanho 6 por definição;
+
+	No fim de tudo o array_vm ficará da seguinte maneira (index/preços):
+	| 0 | 1 | 2 | 3 | 4 | 5  | 6  |
+	| 0 | 2 | 4 | 6 | 8 | 10 | 12 |
+
+	Como o programa terá de devolver o preço máximo do numero de fatias inserido inicialmente, o mesmo será o ultimo valor do array.
+	*)
